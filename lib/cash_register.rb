c06f69570241dfd3978098af6034554f6b614c66
John Million <jpmillion@comcast.net>
2020-11-17 18:27:05 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   end
   
   def add_item(item, price, quanity=1)
-    @total += price * quanity
+    self.total += price * quanity
     quanity.times {@items << item}
     @last_transaction = price * quanity
   end
